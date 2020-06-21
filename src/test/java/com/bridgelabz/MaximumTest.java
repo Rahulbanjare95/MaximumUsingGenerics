7296b48c0c5fec416ab67c35c70cd4476fd8e293
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-21 20:20:55 +0530

    Added:Test case for Maximum at thirdPosition

--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -5,17 +5,24 @@ import org.junit.Test;
 
 public class MaximumTest {
     @Test
-    public void givenMaxAtFirst_ShouldReturnSameNumber (){
+    public void givenMaxAtFirst_ShouldReturnFirstNumber (){
         Maximum maximum=new Maximum();
         Integer highest=maximum.findmax(20,15,10);
         Assert.assertEquals(20,(int)highest);
 
     }
     @Test
-    public void givenMaxAtSecond_ShouldReturnSameNumber (){
+    public void givenMaxAtSecond_ShouldReturnSecondNumber (){
         Maximum maximum=new Maximum();
         Integer highest=maximum.findmax(5,15,10);
         Assert.assertEquals(15,(int)highest);
+
+    }
+    @Test
+    public void givenMaxAtThird_ShouldReturnThirdNumber (){
+        Maximum maximum=new Maximum();
+        Integer highest=maximum.findmax(5,15,50);
+        Assert.assertEquals(50,(int)highest);
 
     }
 
