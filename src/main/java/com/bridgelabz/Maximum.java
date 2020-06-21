2da1c93b740a6d2bff9b20beed3b402af74d4743
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-21 23:31:13 +0530

    Refactored:Maximum to provide Generic implementation of all datatypes

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -2,39 +2,15 @@ package com.bridgelabz;
 
 public class Maximum {
 
-    public Integer findMaxInteger(Integer firstPosition,Integer secondPosition,Integer thirdPosition){
-        Integer max=firstPosition;
-
+    public  static <E extends Comparable<E>> E maximumFinder(E firstPosition,E secondPosition,E thirdPosition){
+        E max=firstPosition;
         if(secondPosition.compareTo(max)>0){
             max=secondPosition;
         }
         if(thirdPosition.compareTo(max)>0){
             max=thirdPosition;
         }
-
-            return max;
+        return max;
     }
 
-
-    public Double findMaxDouble(Double firstPosition, Double secondPosition, Double thirdPosition) {
-        Double maxDouble = firstPosition;
-        if (secondPosition.compareTo(maxDouble) > 0) {
-            maxDouble = secondPosition;
-        }
-        if (thirdPosition.compareTo(maxDouble) > 0) {
-            maxDouble = thirdPosition;
-
-        }
-        return maxDouble;
-    }
-    public String findMaxString(String firsposition,String secondPosition, String thirdPosition){
-        String maxString=firsposition;
-        if(secondPosition.compareTo(maxString)>0){
-            maxString=secondPosition;
-        }
-        if(thirdPosition.compareTo(maxString)>0){
-            maxString=thirdPosition;
-        }
-        return maxString;
-    }
 }
