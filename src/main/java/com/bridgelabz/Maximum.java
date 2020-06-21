3d8d77aec384cc11155996abe92925ab5010e53f
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-21 19:21:36 +0530

    Added:Test case for Maximum at firstPosition

---
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class Maximum {
+
+        public Integer findmax(Integer firsPosition,Integer secondPosition,Integer thirdPosition){
+        Integer max=firsPosition;
+
+        if(secondPosition.compareTo(max)>0){
+            return secondPosition;
+        }
+        if(thirdPosition.compareTo(max)>0){
+            return thirdPosition;
+        }
+        else
+            return max;
+    }
+}
---
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaximumTest {
+    @Test
+    public void givenMaxAtFirst_ShouldReturnSameNumber (){
+        Maximum maximum=new Maximum();
+       Integer highest=maximum.findmax(20,15,10);
+        Assert.assertEquals(20,(int)highest);
+
+    }
+}
