2069e612f60e2f9826684932c012c573f46d8808
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-22 08:37:14 +0530

    Added:PrintMax() to print the maximum by calling inside maxFinder()

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -1,12 +1,18 @@
 package com.bridgelabz;
 
+import javax.sound.midi.Soundbank;
 import java.util.Arrays;
 
-public class Maximum {
-
-    public static <E extends Comparable<E>> E maximumFinder(E[] array) {
+public class Maximum<E> {
+    E[] array;
+    public static  <E extends Comparable<E>> E maximumFinder(E[] array) {
         Arrays.sort(array);
         E max = array[array.length - 1];
+        printMax(max);
         return max;
     }
+    private static <E extends Comparable<E>> void printMax(E max) {
+        System.out.println(max);
+    }
+
 }
