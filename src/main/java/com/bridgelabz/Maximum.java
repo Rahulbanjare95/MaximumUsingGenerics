797807f782ad9e4223d3d1aad30a80578d5f7f16
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-22 16:07:45 +0530

    Refactored: class MaximumTest and class Maximum by using options for sorting

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -1,18 +1,13 @@
 package com.bridgelabz;
-
-import javax.sound.midi.Soundbank;
 import java.util.Arrays;
-
-public class Maximum<E> {
-    E[] array;
-    public static  <E extends Comparable<E>> E maximumFinder(E[] array) {
-        Arrays.sort(array);
-        E max = array[array.length - 1];
+public class Maximum<E > {
+    public E maximumFinder(E... options) {
+        Arrays.sort(options);
+        E max = options[options.length - 1];
         printMax(max);
         return max;
     }
-    private static <E extends Comparable<E>> void printMax(E max) {
+    private void printMax(E max) {
         System.out.println(max);
     }
-
 }
--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -1,68 +1,42 @@
 package com.bridgelabz;
-
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Map;
-
 public class MaximumTest {
+    Maximum maximum=new Maximum();
     @Test
     public void givenMaxAtFirst_ShouldReturnFirstNumber (){
-        Maximum maximum=new Maximum();
-        Integer []array={80,20,50,40};
-        Assert.assertEquals(80,(int)maximum.maximumFinder(array));
+        Assert.assertEquals(80,(int)maximum.maximumFinder(80,20,50,40));
     }
     @Test
     public void givenMaxAtSecond_ShouldReturnSecondNumber (){
-        Maximum maximum=new Maximum();
-        Integer []array={20,80,50,40};
-        Assert.assertEquals(80,(int)maximum.maximumFinder(array));
+        Assert.assertEquals(80,(int)maximum.maximumFinder(20,80,50,40));
     }
     @Test
     public void givenMaxAtThird_ShouldReturnThirdNumber (){
-        Maximum maximum=new Maximum();
-        Integer []array={20,50,80,40};
-        Assert.assertEquals(80,(int)maximum.maximumFinder(array));
-
+        Assert.assertEquals(80,(int)maximum.maximumFinder(20,50,80,40));
     }
     @Test
     public void givenMaxDouble_AtFirst_shouldReturnFirstNumber(){
-        Maximum maximum=new Maximum();
-        Double []array={5.6,2.5,0.5};
-        Assert.assertEquals((Double) 5.6,maximum.maximumFinder(array));
+        Assert.assertEquals((Double) 5.6,maximum.maximumFinder(5.6,2.5,0.5));
     }
     @Test
     public void givenMaxDouble_AtSecond_shouldReturnSecondNumber() {
-        Maximum maximum = new Maximum();
-        Double []array={2.5,5.6,0.5};
-        Assert.assertEquals((Double)5.6,maximum.maximumFinder(array));
+        Assert.assertEquals((Double)5.6,maximum.maximumFinder(2.5,5.6,0.5));
     }
     @Test
     public void givenMaxDouble_AtThird_shouldReturnThirdNumber() {
-        Maximum maximum = new Maximum();
-        Double []array={0.5,2.5,5.6};
-        Assert.assertEquals( (Double)5.6,maximum.maximumFinder(array));
-    }
-
-    @Test
-    public void givenMaxString_AtFirstPosition_ShoulReturnFirstString(){
-        Maximum maximum=new Maximum();
-        String []array={"Peach","Banana","Apple"};
-        Assert.assertEquals("Peach",maximum.maximumFinder(array));
+        Assert.assertEquals( (Double)5.6,maximum.maximumFinder(0.5,2.5,5.6));
     }
     @Test
-    public void givenMaxString_AtSecondPosition_ShoulReturnSecondString(){
-        Maximum maximum=new Maximum();
-        String []array={"Java","Swift","Python"};
-        Assert.assertEquals("Swift",maximum.maximumFinder(array));
+    public void givenMaxString_AtFirstPosition_ShouldReturnFirstString(){
+        Assert.assertEquals("Peach",maximum.maximumFinder("Peach","Banana","Apple"));
     }
     @Test
-    public void givenMaxString_AtThirdPosition_ShoulReturnThirdString(){
-        Maximum maximum=new Maximum();
-        String []array={"Engineering","Commerce","Medicine"};
-        Assert.assertEquals( "Medicine",maximum.maximumFinder(array));
+    public void givenMaxString_AtSecondPosition_ShouldReturnSecondString(){
+        Assert.assertEquals("Swift",maximum.maximumFinder("Java","Swift","Python"));
     }
-
-
+    @Test
+    public void givenMaxString_AtThirdPosition_ShouldReturnThirdString(){
+        Assert.assertEquals( "Medicine",maximum.maximumFinder("Engineering","Commerce","Medicine"));
+    }
 }
-
